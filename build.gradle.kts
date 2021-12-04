ec75ab8daedeb3d86bb1a63b37c1a1de534b66e9
antonshikalyov <anton.shikalyov@gmail.com>
2021-12-04 13:02:35 +0200

    2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     implementation("ch.qos.logback:logback-classic:1.2.6")
     implementation("org.slf4j:slf4j-api:1.7.32")
--- src/main/kotlin/elevator2/Main2.kt
+++ src/main/kotlin/elevator2/Main2.kt
@@ -7,15 +7,15 @@ var currentFloor1: Int = 1
 
 fun main(){
 
-//    do {        // Цикл катания на лифте пока не надоест
+    do {        // Цикл катания на лифте пока не надоест
         Elevator1().elevator()
-//        print("Еще разок?: ")
-//
-//        val exitGame  = Scanner(System.`in`)
-//        val exit = exitGame.next()
-//
-//        println("Лифт на ходиться на $currentFloor1 этаже")
-//    }while (exit != "Finish")
+        print("Еще разок?: ")
+
+        val exitGame  = Scanner(System.`in`)
+        val exit = exitGame.next()
+
+        println("Лифт на ходиться на $currentFloor1 этаже")
+    }while (exit != "Finish")
 }
 
 
@@ -24,28 +24,28 @@ class Elevator1 {
     var nextFloor: Int? = null
 
     fun elevator() {
-            print("Введите следующий этаж: ")
-            nextFloor = input.nextInt()
-            Floor().nextFloor(nextFloor!!)
+        print("Введите следующий этаж: ")
+        nextFloor = input.nextInt()
+        Floor().nextFloor(nextFloor!!)
 
         fun x(){
-                                when (nextFloor) {
-                        1 -> Floor1().nextFloor(nextFloor!!)
-                        2 -> Floor2().nextFloor(nextFloor!!)
-                        3 -> Floor3().nextFloor(nextFloor!!)
-                        4 -> Floor4().nextFloor(nextFloor!!)
+            when (nextFloor) {
+                1 -> Floor1().nextFloor(nextFloor!!)
+                2 -> Floor2().nextFloor(nextFloor!!)
+                3 -> Floor3().nextFloor(nextFloor!!)
+                4 -> Floor4().nextFloor(nextFloor!!)
 //                5 -> Floor.Floor5().nextFloor(nextFloor!!)
 //                6 -> Floor.Floor6().nextFloor(nextFloor!!)
 //                7 -> Floor.Floor7().nextFloor(nextFloor!!)
 //                8 -> Floor.Floor8().nextFloor(nextFloor!!)
 //                9 -> Floor.Floor9().nextFloor(nextFloor!!)
-                    }
+            }
         }
 
         if (myFloor1 == currentFloor1) {         // Алгоритм помогающий ориентироваться
-            println("Лифт уже прибыл!")
-        } else if (nextFloor == currentFloor1) {
             println("Вы и так на месте!")
+        } else if (nextFloor == currentFloor1) {
+            println("Лифт уже прибыл!")
         }
 
         if (0 < nextFloor!! && nextFloor!! < 10) {
@@ -95,11 +95,11 @@ class Floor1(): Floor() {
         set(nextFloor) {
             nextFloor1 = nextFloor
         }
-        fun get() = numberOfFloor
+    fun get() = numberOfFloor
 
     override fun nextFloor(nextFloor2: Int) {
-       super.nextFloor(nextFloor1!!)
-   }
+        nextFloor(nextFloor1!!)
+    }
 }
 
 class Floor2(): Floor() {
@@ -116,30 +116,30 @@ class Floor2(): Floor() {
     }
 }
 
-    class Floor3(): Floor() {
-        override var numberOfFloor: Int = 3;
-        override var nextFloor1: Int?
-            get() = super.nextFloor1
-            set(nextFloor) {
-                nextFloor1 = nextFloor1
-            }
-
-        override fun nextFloor(nextFloor2: Int) {
-            nextFloor(nextFloor1!!)
+class Floor3(): Floor() {
+    override var numberOfFloor: Int = 3;
+    override var nextFloor1: Int?
+        get() = super.nextFloor1
+        set(nextFloor) {
+            nextFloor1 = nextFloor1
         }
+
+    override fun nextFloor(nextFloor2: Int) {
+        nextFloor(nextFloor1!!)
     }
+}
 
-    class Floor4(): Floor() {
-        override var numberOfFloor: Int = 4
-        override var nextFloor1: Int?
-            get() = super.nextFloor1
-            set(nextFloor) {
-                nextFloor1 = nextFloor1
-            }
-        fun get() = numberOfFloor
-
-        override fun nextFloor(nextFloor2: Int) {
-            super.nextFloor(nextFloor1!!)
+class Floor4(): Floor() {
+    override var numberOfFloor: Int = 4
+    override var nextFloor1: Int?
+        get() = super.nextFloor1
+        set(nextFloor) {
+            nextFloor1 = nextFloor1
         }
+    fun get() = numberOfFloor
+
+    override fun nextFloor(nextFloor2: Int) {
+        super.nextFloor(nextFloor1!!)
     }
+}
 
